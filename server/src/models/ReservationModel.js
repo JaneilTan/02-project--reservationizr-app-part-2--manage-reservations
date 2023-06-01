b4c299758a20ae8f4d1eaa71402298a541932f3d
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-06-02 11:48:24 +1200

    Added schema for Reservation

--- server/src/models/ReservationModel.js
+++ server/src/models/ReservationModel.js
@@ -1,1 +1,17 @@
 // FIXME: Add a Mongoose model here
+const mongoose = require ("mongoose");
+const { Schema } = mongoose;
+
+const reservationSchema = Schema({ 
+  partySize: {type: Number, required: true},
+  date: { type: Date, default: Date.now, required:true },
+  userId: {type: String, required: true},
+  restaurantName: {type: String, required: true},
+ 
+});
+
+
+
+const ReservationModel = mongoose.model("Reservation", reservationSchema);
+
+module.exports = ReservationModel;
