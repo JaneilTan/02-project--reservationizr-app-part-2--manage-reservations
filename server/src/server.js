beeb7c5be892283e0e905284afd627838516aae7
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-06-02 11:47:24 +1200

    connect to mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,9 @@
+const mongoose = require("mongoose");
 const port = process.env.PORT || 5001;
 const app = require("./app");
+
+
+mongoose.connect("mongodb://127.0.0.1:27017/mongo");
 
 app.listen(port, () => {
   console.log(`API server started at http://localhost:${port}`);
