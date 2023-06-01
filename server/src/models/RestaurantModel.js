cef837e374af7adcd03e10de1f29f8900a79f0bc
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-06-02 11:48:55 +1200

    Added schema for Restaurant

--- server/src/models/RestaurantModel.js
+++ server/src/models/RestaurantModel.js
@@ -1,1 +1,17 @@
 // FIXME: Add a Mongoose model here
+const mongoose = require ("mongoose");
+const { Schema } = mongoose;
+
+const restaurantSchema = Schema({ 
+  id: {type: String, required: true},
+  name: { type: String, required:true },
+  description: {type: String, required: true},
+  image: {type: String, required: true},
+ 
+});
+
+
+
+const RestaurantModel = mongoose.model("Restaurant", restaurantSchema);
+
+module.exports = RestaurantModel;
