c95513cf2b8881df3fd834c8a59209c7a4410745
Janeil Tan <janeil.tan@developersinstitute.co.nz>
2023-07-03 14:31:18 +1200

    Added environment variables

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5001;
 const app = require("./app");
+const mongoDbUri = process.env.MONGO_URI || "mongodb:localhost:27017/mongo";
 
-
-mongoose.connect("mongodb://127.0.0.1:27017/mongo");
+mongoose.connect(mongoDbUri);
 
 app.listen(port, () => {
   console.log(`API server started at http://localhost:${port}`);
